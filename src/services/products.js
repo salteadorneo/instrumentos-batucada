3dac038da7b92b1618bea0bf2471396fbb1291bf
Cristian Adán <cristian@doersdf.com>
2023-03-22 17:05:15 +0100

    search tag affiliate

--- src/services/products.js
+++ src/services/products.js
@@ -1,6 +1,17 @@
+const TAG_AFFILIATE = 'neo0ba-21';
+
 import pkg from "unofficial-amazon-search";
 const { searchAmazon } = pkg;
 
 export async function searchProducts(query) {
-  return await searchAmazon(query);
+  const response = await searchAmazon(query);
+    return response?.searchResults?.map(result => {
+        const url = new URL(`https://www.amazon.es${result.productUrl}`);
+        url.searchParams.set('tag', TAG_AFFILIATE);
+
+        return {
+            ...result,
+            productUrl: url,
+        }
+    });
 };
