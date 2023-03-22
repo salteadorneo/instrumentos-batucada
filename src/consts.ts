cf589f013d860a8bbe90832404821f7247d0122a
salteadorneo <hola@salteadorneo.dev>
2023-03-22 23:47:53 +0100

    change meta description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Instrumentos de batucada';
-export const SITE_DESCRIPTION = 'Instrumentos de percusión para batucada';
+export const SITE_DESCRIPTION = 'Encuentra información detallada sobre cada tipo de instrumento, desde surdos hasta tamborines. ¡Sumérgete en el mundo de la batucada y disfruta de la música!';
