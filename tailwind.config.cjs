670bcccbdd5fdde86ab199e4f99b6b5edaab1e14
Cristian Adán <hola@salteadorneo.dev>
2023-04-04 19:52:09 +0200

    css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: "var(--color-primary)",
-			},
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)'
+      }
+    }
+  },
+  plugins: []
 }
