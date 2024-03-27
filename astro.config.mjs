38fbac9e3df72c2d50236fa28be8fe6b43634887
Cristian Adán <hola@salteadorneo.dev>
2024-03-27 20:18:44 +0100

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
-import vercel from "@astrojs/vercel/static";
+import vercel from '@astrojs/vercel/static'
 
 export default defineConfig({
   site: 'https://instrumentos-batucada.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "static",
   adapter: vercel()
-});
+})
